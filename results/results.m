78651285c90a6ec06bf873a6b779ee536ff6cb06
Michael Wang <michael20995@gmail.com>
2020-05-02 11:36:18 -0400

    updated hcp tan

--- results/results.m
+++ results/results.m
@@ -4,7 +4,7 @@ test_only = csvread('HCP100_E200_LR0.001_R0_S0_Y1_test.csv', 1, 0);
 pct_comps = all_data(:,1);
 accuracies = [all_data(:,2), test_only(:,2)];
 losses = [all_data(:,3), test_only(:,3)];
-
+mean(accuracies
 fig1 = subplot(1,2,1);
 set(gcf, 'Position',  [100, 100, 1250, 500])
 plot(pct_comps, losses, '.', 'MarkerSize',15);
@@ -30,6 +30,7 @@ rep5 = csvread('HCP100_E200_LR0.001_R0_S0_Y1_5_test.csv',1,0);
 
 pct_comps = rep1(:,1);
 accuracy = [rep2(:,2), rep3(:,2), rep4(:,2), rep5(:,2)];
+mean(accuracy(1,:))
 loss = [rep2(:,3), rep3(:,3), rep4(:,3), rep5(:,3)];
 
 fig2 = subplot(1,2,1);
